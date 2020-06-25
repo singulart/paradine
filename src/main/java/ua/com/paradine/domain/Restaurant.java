79f4346a0d64cbdf087d89a3d79b87cd2af8cdc5
singulart <serpian@gmail.com>
2020-06-25 14:42:57 +0300

    added Ehcache provider

--- src/main/java/ua/com/paradine/domain/Restaurant.java
+++ src/main/java/ua/com/paradine/domain/Restaurant.java
@@ -1,5 +1,7 @@
 package ua.com.paradine.domain;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import javax.persistence.*;
 import javax.validation.constraints.*;
@@ -15,6 +17,7 @@ import java.util.Set;
  */
 @Entity
 @Table(name = "restaurant")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 @org.springframework.data.elasticsearch.annotations.Document(indexName = "restaurant")
 public class Restaurant implements Serializable {
 
@@ -80,6 +83,7 @@ public class Restaurant implements Serializable {
     private String uuid;
 
     @OneToMany(mappedBy = "restaurant")
+    @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
     private Set<PopularTime> popularTimes = new HashSet<>();
 
     // jhipster-needle-entity-add-field - JHipster will add fields here
