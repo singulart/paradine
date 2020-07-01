9b8496a3d506be6f8950a55c7ba098510481727d
singulart <serpian@gmail.com>
2020-07-01 17:52:44 +0300

    fixed strange JPA issue about the named parameter

--- src/main/java/ua/com/paradine/core/dao/ExtendedPopularTimeRepository.java
+++ src/main/java/ua/com/paradine/core/dao/ExtendedPopularTimeRepository.java
@@ -2,11 +2,12 @@ package ua.com.paradine.core.dao;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ua.com.paradine.domain.PopularTime;
 import ua.com.paradine.repository.PopularTimeRepository;
 
 public interface ExtendedPopularTimeRepository extends PopularTimeRepository {
 
     @Query(value = "FROM PopularTime pt WHERE pt.restaurant.id in (:restaurantIds)")
-    List<PopularTime> fetchByRestaurantIdIn(List<Long> restaurantIds);
+    List<PopularTime> fetchByRestaurantIdIn(@Param("restaurantIds") List<Long> restaurantIds);
 }
