7b8ee350a2065cd852f604dffd638bd3a8029650
singulart <serpian@gmail.com>
2020-07-12 01:33:07 +0300

    Fixing timezone offset-related bugs in submission flow

--- src/test/java/ua/com/paradine/core/Nowness.java
+++ src/test/java/ua/com/paradine/core/Nowness.java
@@ -1,5 +1,18 @@
 package ua.com.paradine.core;
 
+import static ua.com.paradine.core.ParadineConstants.DEFAULT_ZONE;
+
+import java.time.OffsetDateTime;
+import java.time.ZonedDateTime;
+
 public class Nowness {
+
+    public static OffsetDateTime getNow() {
+        return ZonedDateTime.now(DEFAULT_ZONE).toOffsetDateTime();
+    }
+
+    public static ZonedDateTime getNowZoned() {
+        return ZonedDateTime.now(DEFAULT_ZONE);
+    }
 
 }
