09b6b2c609fe79b6504e6b4f6337a835cc0a8241
singulart <serpian@gmail.com>
2020-08-20 18:16:50 +0300

    production database SQL data

---
+++ db_data/PARADINE_PROD_PUBLIC_CITY.sql
@@ -0,0 +1,4 @@
+INSERT INTO PUBLIC.CITY (ID, SLUG, NAME, IMAGE) VALUES (1, 'kyiv', 'Kyiv', 'https://cdn.thecrazytourist.com/wp-content/uploads/2018/08/ccimage-shutterstock_504232516.jpg');
+INSERT INTO PUBLIC.CITY (ID, SLUG, NAME, IMAGE) VALUES (2, 'kharkiv', 'Kharkiv', 'https://cdn.thecrazytourist.com/wp-content/uploads/2018/08/ccimage-shutterstock_528449170.jpg');
+INSERT INTO PUBLIC.CITY (ID, SLUG, NAME, IMAGE) VALUES (3, 'odessa', 'Odessa', 'https://cdn.thecrazytourist.com/wp-content/uploads/2018/08/ccimage-shutterstock_559208056.jpg');
+INSERT INTO PUBLIC.CITY (ID, SLUG, NAME, IMAGE) VALUES (4, 'lviv', 'Lviv', 'https://cdn.thecrazytourist.com/wp-content/uploads/2018/08/ccimage-shutterstock_342499724.jpg');
